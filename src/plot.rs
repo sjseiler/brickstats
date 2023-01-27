ae2bc001f2699511fe7e4c908483d21a7475048a
Sebastian Seiler <sebastian.seiler@posteo.de>
2023-01-27 09:49:44 +0100

    refactor and remove warnings

--- src/plot.rs
+++ src/plot.rs
@@ -1,173 +1,91 @@
-#[derive(Debug, Clone, PartialEq, Eq, Hash, PartialOrd, Ord)]
-pub struct Label(String);
+use anyhow::{Error, Result};
+use std::io::{BufWriter, Write};
+use std::process::{Command, Stdio};
 
-impl Label {
-    pub fn new(label: &str) -> Label {
-        Label(label.to_string())
-    }
+// convert to png with something similar to >> set terminal pngcairo enhanced font \"Times New Roman,12.0\" size 1500,1100
+pub const DEFAULT_CONFIG: &str = "
+set palette rgbformulae 3,2,2
+set palette maxcolors 256
+unset colorbox
+set style data histograms
+set style histogram rowstacked
+set style fill solid border -1
+set boxwidth 0.9 relative
+set key autotitle columnheader 
+set ytics 10
+#set mytics 5
+set xtics rotate by -45 scale 0
+set grid ytics";
+
+/// gnuplot object
+pub struct Gnuplot {
+    // gnuplot process
+    // process: Child,
+    // gnuplot stdin
+    // stdin: BufWriter<ChildStdin>,
 }
 
-#[derive(Debug, Clone, PartialEq)]
-pub struct Data {
-    value: f64,
-    label: Option<Label>,
-}
-
-impl Data {
-    #[allow(dead_code)]
-    pub fn new(value: f64) -> Data {
-        Data { value, label: None }
-    }
-
-    pub fn new_with_label(value: f64, label: &str) -> Data {
-        Data {
-            value,
-            label: Some(Label::new(label)),
+impl Gnuplot {
+    pub fn show(
+        config: &str,
+        labels: Vec<String>,
+        data: Vec<Vec<i32>>,
+        colors: Vec<String>,
+    ) -> Result<()> {
+        let mut color_iter = colors.iter();
+        // generate config string
+        let mut config_string = config.to_string();
+        config_string.push_str(&format!(
+            "\nplot '-' using 2:xtic(1) with histogram notitle lc rgb \"#{}\",",
+            color_iter.next().expect("didn't get enough colors")
+        ));
+        for _ in data.iter().skip(1) {
+            config_string.push_str(&format!(
+                " '-' using 2 with histogram notitle lc rgb \"#{}\",",
+                color_iter.next().expect("didn't get enough colors")
+            ));
         }
-    }
-}
+        // remove last comma
+        config_string.pop();
 
-#[allow(dead_code)]
-pub struct Bins {
-    bin_width: f64,
-    bin_count: usize,
-    bins: Vec<Label>,
-}
-
-pub struct Histogram {
-    data: Vec<Data>,
-    bins: Bins,
-}
-
-/// bins of a histogram
-/// bins can be created from data values or labels
-impl Bins {
-    /// create bins from data values
-    /// bin_count: number of bins
-    /// bin labels will be generated automatically
-    #[allow(dead_code)]
-    pub fn new_from_values(data: &Vec<Data>, bin_count: usize) -> Bins {
-        let mut min = data[0].value;
-        let mut max = data[0].value;
-        for d in data {
-            if d.value < min {
-                min = d.value;
-            }
-            if d.value > max {
-                max = d.value;
-            }
+        let mut data_string = String::new();
+        // generate data string from labels and data
+        // make iterator from data
+        for d in data.iter() {
+            let mut data = labels
+                .iter()
+                .zip(d.iter())
+                .map(|(label, value)| format!("\"{}\" {}", label, value))
+                .collect::<Vec<String>>()
+                .join("\n");
+            // add end of data marker
+            data.push_str("\ne\n");
+            data_string.push_str(&data);
         }
-        let bin_width = (max - min) / bin_count as f64;
-        let mut bins = Vec::new();
-        for i in 0..bin_count {
-            let bin_min = min + i as f64 * bin_width;
-            let bin_max = min + (i + 1) as f64 * bin_width;
-            let bin_label = format!("{} - {}", bin_min, bin_max);
-            bins.push(Label(bin_label));
-        }
-        Bins {
-            bin_width,
-            bin_count,
-            bins,
-        }
-    }
 
-    /// create bins from data labels
-    /// bin_count: number of bins
-    /// labels will be used as bins
-    pub fn new_from_labels(data: &Vec<Data>) -> Bins {
-        let mut bins = Vec::new();
-        for d in data {
-            if let Some(label) = &d.label {
-                if !bins.contains(label) {
-                    bins.push(label.clone());
+        // print config and data
+        // println!("{}", config_string);
+        // println!("{}", data_string);
+
+        let mut process = Command::new("gnuplot")
+            .arg("-p")
+            .stdin(Stdio::piped())
+            .spawn()
+            .expect("Couldn't spawn gnuplot. Make sure it is installed and available in PATH.");
+        let mut stdin = BufWriter::new(process.stdin.take().unwrap());
+        writeln!(stdin, "{}", config_string)?;
+        writeln!(stdin, "{}", data_string)?;
+        stdin.flush()?;
+
+        match process.wait() {
+            Ok(status) => {
+                if status.success() {
+                    Ok(())
+                } else {
+                    Err(Error::msg("gnuplot exited with non-zero status"))
                 }
             }
-        }
-        Bins {
-            bin_width: 1.0,
-            bin_count: bins.len(),
-            bins,
-        }
-    }
-}
-
-impl Histogram {
-    // create histogram from data and bins
-    pub fn new(data: &Vec<Data>, bins: Bins) -> Self {
-        let histogram = Histogram {
-            data: data.clone(),
-            bins: bins,
-        };
-        histogram
-    }
-
-    // plot histogram
-    // align labels to the left and bars to the right
-    #[allow(dead_code)]
-    pub fn plot(&self) {
-        let mut counts = vec![0; self.bins.bin_count];
-        for d in &self.data {
-            let bin_index = d.value as usize;
-            counts[bin_index] += 1;
-        }
-        let max_count = counts.iter().max().unwrap();
-        let max_count_len = max_count.to_string().len();
-        let max_label_len = self.bins.bins.iter().map(|l| l.0.len()).max().unwrap();
-        let max_bar_len = 80 - max_count_len - max_label_len - 4;
-        for i in 0..self.bins.bin_count {
-            let count = counts[i];
-            let bar_len = (count as f64 / *max_count as f64 * max_bar_len as f64) as usize;
-            let bar = "=".repeat(bar_len);
-            println!(
-                "{:width$} | {:>width2$} | {}",
-                self.bins.bins[i].0,
-                count,
-                bar,
-                width = max_label_len,
-                width2 = max_count_len
-            );
-        }
-    }
-
-    // plot weighted histogram (data values are weights and added up for each bin)
-    // align labels to the left and bars to the right
-    pub fn plot_weighted(&self) {
-        let mut weighted_labels = Vec::new();
-        for label in &self.bins.bins {
-            // sum all values from data where label matches
-            let sum = self
-                .data
-                .iter()
-                .filter(|d| d.label == Some(label.clone()))
-                .map(|d| d.value)
-                .sum::<f64>();
-            weighted_labels.push((label.clone(), sum.round() as i32));
-        }
-
-        // sort weighted labels by label string
-        weighted_labels.sort_by(|a, b| a.0.cmp(&b.0));
-        // weighted_labels.sort_by(|a, b| b.1.cmp(&a.1));
-
-        let max_count = weighted_labels.iter().map(|(_, c)| c).max().unwrap();
-        let max_count_len = max_count.to_string().len();
-        let max_label_len = weighted_labels
-            .iter()
-            .map(|(l, _)| l.0.len())
-            .max()
-            .unwrap();
-        let max_bar_len = 80 - max_count_len - max_label_len - 4;
-        for (label, count) in &weighted_labels {
-            let bar_len = (*count as f64 / *max_count as f64 * max_bar_len as f64) as usize;
-            let bar = "=".repeat(bar_len);
-            println!(
-                "{:width$} | {:>width2$} | {}",
-                label.0,
-                count,
-                bar,
-                width = max_label_len,
-                width2 = max_count_len
-            );
+            Err(e) => Err(Error::msg(format!("gnuplot failed to run: {}", e))),
         }
     }
 }
---
+++ src/stats.rs
@@ -0,0 +1,20 @@
+use crate::rebrickable::{inventory_part, part_details};
+
+// calculate average age of parts in inventory
+#[allow(dead_code)]
+pub fn average_part_year(
+    inventory_parts: &Vec<inventory_part>,
+    part_details: &Vec<part_details>,
+) -> f32 {
+    let mut average_year = 0.0;
+    let mut part_count = 0;
+
+    // zip inventory_parts and part_details
+    let tuples = inventory_parts.iter().zip(part_details.iter());
+
+    for (inventory_part, part_details) in tuples {
+        average_year += part_details.get_year_from() as f32 * inventory_part.get_quantity() as f32;
+        part_count += inventory_part.get_quantity();
+    }
+    average_year / part_count as f32
+}
